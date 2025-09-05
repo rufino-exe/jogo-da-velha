b59fc8139b612ab43341b51b7c48091319e7b958
rufino-exe <rafaelceschirufino@gmail.com>
2025-09-04 21:45:58 -0300

    Funcionamento do método de vencedor.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ export default function Tabuleiro(){
 
   function handleClick(i){
     const nextSquares = squares.slice()
+
+    if(nextSquares[i] || calculaVencedor(squares)){
+      return
+    }
     if(xIsNext){
       nextSquares[i] = 'X'
       setXIsNext(false)
@@ -45,4 +49,17 @@ export default function Tabuleiro(){
       </div>
     </div>
   );
+}
+function calculaVencedor(squares){
+  const lines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+                [1,4,3],[2,5,8], [0,4,8], [2,4,6]]
+
+  for( let i = 0; i<lines.length; i++){
+    const [a,b,c] = lines[i]
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+    return squares[a]
+    }
+  }
+    return null
 }
